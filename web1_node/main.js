0f56fe67db2c37f051d13a42b65419870bbbf7b7
sknrchoi <seulkinara@gmail.com>
2019-04-10 00:47:56 +0900

    생활코딩 nodejs강의 내용

---
+++ web1_node/main.js
@@ -0,0 +1,85 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+ 
+var app = http.createServer(function(request,response){
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    var pathname = url.parse(_url, true).pathname;
+    /*if(_url == '/'){
+      title = 'Welcome';
+    }
+    if(_url == '/favicon.ico'){
+      return response.writeHead(404);
+    }*/
+
+    console.log(url.parse(_url, true).pathname);
+
+    if (pathname === '/') {
+        if (queryData.id === undefined) {
+            var title = 'Welcome';
+            var description = 'Hello node js';
+            var template = `
+            <!doctype html>
+            <html>
+            <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+            </head>
+            <body>
+            <h1><a href="/">WEB</a></h1>
+            <ul>
+                <li><a href="/?id=HTML">HTML</a></li>
+                <li><a href="/?id=CSS">CSS</a></li>
+                <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ul>
+            <h2>${title}</h2>
+            <p><a href="https://www.w3.org/TR/html5/" target="_blank" title="html5 speicification">Hypertext Markup Language (HTML)</a> is the standard markup language for <strong>creating <u>web</u> pages</strong> and web applications.Web browsers receive HTML documents from a web server or from local storage and render them into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
+            <img src="coding.jpg" width="100%">
+            </p>
+            <p style="margin-top:45px;">
+                ${description}
+            </p>
+            </body>
+            </html>
+            `;
+            response.writeHead(200);   
+            response.end(template);
+        } else {
+            fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
+                var title = queryData.id;
+                var template = `
+                <!doctype html>
+                <html>
+                <head>
+                <title>WEB1 - ${title}</title>
+                <meta charset="utf-8">
+                </head>
+                <body>
+                <h1><a href="/">WEB</a></h1>
+                <ul>
+                    <li><a href="/?id=HTML">HTML</a></li>
+                    <li><a href="/?id=CSS">CSS</a></li>
+                    <li><a href="/?id=JavaScript">JavaScript</a></li>
+                </ul>
+                <h2>${title}</h2>
+                <p><a href="https://www.w3.org/TR/html5/" target="_blank" title="html5 speicification">Hypertext Markup Language (HTML)</a> is the standard markup language for <strong>creating <u>web</u> pages</strong> and web applications.Web browsers receive HTML documents from a web server or from local storage and render them into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
+                <img src="coding.jpg" width="100%">
+                </p>
+                <p style="margin-top:45px;">
+                    ${description}
+                </p>
+                </body>
+                </html>
+                `;
+                response.writeHead(200);   
+                response.end(template);
+            });           
+        }
+        
+    } else {   
+        response.writeHead(404);   
+        response.end('Not found');
+    }   
+});
+app.listen(3000);
