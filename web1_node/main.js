11a945ea3d5ec2d9de69e75321daa16d0496cfa3
sknrchoi <seulkinara@gmail.com>
2019-04-24 13:37:56 +0900

    feat: add security coding about input path data
    File path is parsed using path module. Parse function returns object included root, dir, base, ext, name. I used base property and change queryData.id to filteredId.

--- web1_node/main.js
+++ web1_node/main.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 var template = require('./lib/template.js');
+var path = require('path');
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
@@ -30,7 +31,11 @@ var app = http.createServer(function(request,response){
             
         } else {
             fs.readdir('./data', function(error, filelist) {
-                fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
+                var filteredId = path.parse(queryData.id).base;
+                console.log('queryData.id = ' + queryData.id);
+                console.log('filteredId = ' + filteredId);
+                
+                fs.readFile(`data/${filteredId}`, 'utf8', function(err, description) {
                     var title = queryData.id;
                     var list = template.list(filelist);
                     var html = template.html(title, list, `<h2>${title}</h2>${description}`,
@@ -98,7 +103,9 @@ var app = http.createServer(function(request,response){
         });
     } else if(pathname === '/update') {
         fs.readdir('./data', function(error, filelist) {
-            fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
+            var filteredId = path.parse(queryData.id).base;
+
+            fs.readFile(`data/${filteredId}`, 'utf8', function(err, description) {
                 var title = queryData.id;
                 var list = template.list(filelist);
                 var description = `
@@ -158,8 +165,9 @@ var app = http.createServer(function(request,response){
         request.on('end', function() {
             var post = qs.parse(body);
             var id = post.id;
+            var filteredId = path.parse(id).base;
 
-            fs.unlink(`data/${id}`, function(err) {
+            fs.unlink(`data/${filteredId}`, function(err) {
                 response.writeHead(302, {Location : `/`});   
                 response.end();
             })
