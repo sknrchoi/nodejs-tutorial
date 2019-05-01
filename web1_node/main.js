dca49ad7abb6d9d8e6e6c3d55d82a3623b331b96
sknrchoi <seulkinara@gmail.com>
2019-05-01 23:37:07 +0900

    refactor: Move database query code in main.js to topic.js in lib directory
    This check-in version includes the function to express author information.

--- web1_node/main.js
+++ web1_node/main.js
@@ -1,173 +1,31 @@
 var http = require('http');
 var url = require('url');
-var qs = require('querystring');
-var template = require('./lib/template.js');
-var path = require('path');
-var db = require('./lib/db');
+var topic = require('./lib/topic');
 
-var app = http.createServer(function(request,response){
+var app = http.createServer(function (request, response) {
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
-    
 
     if (pathname === '/') {
+
         if (queryData.id === undefined) {
-            db.query(`SELECT * FROM topic`, function(error, topics) {
-                var title = 'Welcome';
-                var description = 'Hello node js';
-                var list = template.list(topics);
-                var html = template.html(title, list,
-                    `<h2>${title}</h2>${description}`,
-                    `<a href="/create">create</a>`);
-                response.writeHead(200);   
-                response.end(html);
-            });
+            topic.home(request, response);
         } else {
-            db.query(`SELECT * FROM topic`, function(error, topics) {
-                if(error) {
-                    throw error;
-                }
-                
-                db.query(`SELECT * FROM topic WHERE id=?`,[queryData.id] ,function(error2, topic) {
-                    if(error2) {
-                        throw error2;
-                    }
-
-                    var title = topic[0].title;
-                    var description = topic[0].description;
-                    var list = template.list(topics);
-                    var html = template.html(title, list, `<h2>${title}</h2>${description}`,
-                    `<a href="/create">create</a>
-                     <a href="/update?id=${queryData.id}">update</a>
-                     <form action="/delete_process" method="post">
-                         <input type="hidden" name="id" value="${queryData.id}">
-                         <input type="submit" value="delete">
-                     </form>
-                    `
-                    );
-                    response.writeHead(200);   
-                    response.end(html);
-
-                });
-            });
+            topic.page(request, response);
         }
-        
-    } else if(pathname === '/create') {
-        db.query(`SELECT * FROM topic`, function(error, topics) {
-            var title = 'WEB - create';
-            var list = template.list(topics);
-            var html = template.html(title, list,
-                `<form action="http://localhost:3000/process_create" method="post">
-                    <p><input type="text" name="title" placeholder="title"></p>
-                    <p>
-                        <textarea name="description" placeholder="description"></textarea>
-                    </p>
-                    <p>
-                        <input type="submit">
-                    </p>
-                </form>`,
-                ``);
-            response.writeHead(200);   
-            response.end(html);
-        });
-
-    } else if(pathname === '/process_create') {
-        var body = "";
-        // data이벤트에서 발생시킨 청크는 buffer로 문자열 데이터임.
-        request.on('data', function(data) {
-            body = body + data;
-        });
-        // 위에서 수집한 데이터를 파싱해서 body데이터를 가져옴.
-        request.on('end', function() {
-            var post = qs.parse(body);
-            // post == { title: '3243', description: '24234234' }
-
-            db.query(`INSERT INTO topic (title, description, created, author_id) VALUES(?, ?, NOW(), ?)`, 
-            [post.title, post.description, 1], function(error, result) {
-                if(error) {
-                    throw error;
-                }
-                
-                response.writeHead(302, {Location: `/?id=${result.insertId}`});   
-                response.end();
-            });
-
-        });
-    } else if(pathname === '/update') {
-        db.query(`SELECT * FROM topic`, function(error, topics) {
-            if(error) {
-                throw error;
-            }
-            
-            db.query(`SELECT * FROM topic WHERE id=?`,[queryData.id] ,function(error2, topic) {
-                if(error2) {
-                    throw error2;
-                }
-
-                var list = template.list(topics);
-                var html = template.html(topic[0].title, list, 
-                    `<form action="/process_update" method="post">
-                        <input type="hidden" name="id" value="${topic[0].id}">
-                        <p><input type="text" name="title" placeholder="title" value="${topic[0].title}"></p>
-                        <p>
-                            <textarea name="description" placeholder="description">
-                                ${topic[0].description}
-                            </textarea>
-                        </p>
-                        <p>
-                            <input type="submit">
-                        </p>
-                    </form>`,
-                    `<a href="/create">create</a><a href="/update?id=${topic[0].id}">update</a>`
-                );
-                response.writeHead(200);   
-                response.end(html);
-
-            });
-        });
-        
-    } else if(pathname === '/process_update') {
-        var body = "";
-        // data이벤트에서 발생시킨 청크는 buffer로 문자열 데이터임.
-        request.on('data', function(data) {
-            body = body + data;
-        });
-        // 위에서 수집한 데이터를 파싱해서 body데이터를 가져옴.
-        request.on('end', function() {
-            var post = qs.parse(body);
-
-            db.query(`UPDATE topic SET title=?, description=?, created=NOW(), author_id=1 WHERE id=?`, 
-            [post.title, post.description, post.id], function(error, result) {
-                response.writeHead(302, {Location: `/?id=${post.id}`});   
-                response.end();
-            });
-
-        });
-    } else if(pathname === '/delete_process') {
-        var body = "";
-        // data이벤트에서 발생시킨 청크는 buffer로 문자열 데이터임.
-        request.on('data', function(data) {
-            body = body + data;
-        });
-        // 위에서 수집한 데이터를 파싱해서 body데이터를 가져옴.
-        request.on('end', function() {
-            var post = qs.parse(body);
-            var id = post.id;
-            var filteredId = path.parse(id).base;
-
-            db.query(`DELETE FROM topic WHERE id=?`, 
-            [post.id], function(error, result) {
-                if(error) {
-                    throw error;
-                }
-
-                response.writeHead(302, {Location: `/`});   
-                response.end();
-            });
-        });
-    } else {   
-        response.writeHead(404);   
+    } else if (pathname === '/create') {
+        topic.create(request, response);
+    } else if (pathname === '/process_create') {
+        topic.create_process(request, response);
+    } else if (pathname === '/update') {
+        topic.update(request, response);
+    } else if (pathname === '/process_update') {
+        topic.update_process(request, response);
+    } else if (pathname === '/delete_process') {
+        topic.delete(request, response);
+    } else {
+        response.writeHead(404);
         response.end('Not found');
     }
 });
